7e8925cfccf1ce826782a0b71f7452bcdcb04ad3
kinz <officezsx@gmail.com>
2023-08-19 18:26:10 +0800

    initial database sql

---
+++ table.sql
@@ -0,0 +1,27 @@
+create table t_order_0
+(
+    order_id     varchar(16),
+    user_id      varchar(16),
+    gmt_create   timestamp,
+    gmt_modified timestamp
+);
+
+
+create table t_order_1
+(
+    order_id     varchar(16),
+    user_id      varchar(16),
+    gmt_create   timestamp,
+    gmt_modified timestamp
+);
+
+
+create table t_order_2
+(
+    order_id     varchar(16),
+    user_id      varchar(16),
+    gmt_create   timestamp,
+    gmt_modified timestamp
+);
+
+

e57197a12f4e29dc2a134b384708d4f7b04f66f3
kinz <officezsx@gmail.com>
2023-08-20 11:24:52 +0800

    add order item table init sql

--- table.sql
+++ table.sql
@@ -24,4 +24,32 @@ create table t_order_2
     gmt_modified timestamp
 );
 
+create table t_order_item_0
+(
+    item_id      varchar(16),
+    order_id     varchar(16),
+    product_id   varchar(16),
+    item_nums    int,
+    gmt_create   timestamp,
+    gmt_modified timestamp
+);
 
+create table t_order_item_1
+(
+    item_id      varchar(16),
+    order_id     varchar(16),
+    product_id   varchar(16),
+    item_nums    int,
+    gmt_create   timestamp,
+    gmt_modified timestamp
+);
+
+create table t_order_item_2
+(
+    item_id      varchar(16),
+    order_id     varchar(16),
+    product_id   varchar(16),
+    item_nums    int,
+    gmt_create   timestamp,
+    gmt_modified timestamp
+);
